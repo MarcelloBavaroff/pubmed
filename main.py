81b015963a17f7c5502ad4318e574edb9e0fcd1a
Marcello <marcello.bavaro@mail.polimi.it>
2023-01-11 23:37:42 +0100

    add main

---
+++ main.py
@@ -0,0 +1,15 @@
+import gzip
+import shutil
+import os
+import subprocess
+
+
+def extract(filename):
+    with gzip.open("gz/" + filename + ".gz", 'rb') as f_in:
+        with open("xml/" + filename, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+
+#extract("pubmed23n0001.xml")
+subprocess.run(["extractTIABs.py", "xml/pubmed23n0001.xml"], shell=True)
+
